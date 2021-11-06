8c4f849e4216eac05509ece947ab20b91b7039ca
isaacruizgavilan4geeks <isaacruizgavilan@me.com>
2021-11-06 11:05:19 +0000

    first commit

---
+++ src/models.py
@@ -0,0 +1,76 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from eralchemy import render_er
+
+Base = declarative_base()
+
+class Person(Base):
+    __tablename__ = 'person'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Address(Base):
+    __tablename__ = 'address'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    street_name = Column(String(250))
+    street_number = Column(String(250))
+    post_code = Column(String(250), nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+
+class User(Base):
+    __tablename__: 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250))
+    user_from_id = relationship(Follower)
+    user_to_id = relationship(Follower)
+    post_id= relationship(Post)
+    author_comment_id= relationship(Comment)
+
+class Follower(Base):
+    __tablename__: 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+
+class Post(Base):
+    __tablename__: 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comment_id = relationship(Comment)
+    media_id = relationship(Media)
+
+class Comment(Base):
+    __tablename__: 'comment'
+    id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Media(Base):
+    __tablename__: 'media'
+    id = Column(Integer, primary_key=True)
+    type= Column(String(250))
+    url= Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id')) 
+
+
+    def to_dict(self):
+        return {}
+
+## Draw from SQLAlchemy base
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
