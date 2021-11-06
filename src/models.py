3da30fe0dd49e657cb4cb23958b1f45c201423cf
isaacruga <86105117+isaacruga@users.noreply.github.com>
2021-11-06 12:12:53 +0100

    finished

--- src/models.py
+++ src/models.py
@@ -8,24 +8,6 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
 class Follower(Base):
     __tablename__= 'follower'
     id = Column(Integer, primary_key=True)
